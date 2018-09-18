a1f2b503d1ede27f78cf7f5d216f4c7683dd0b4f
arthiverma03 <arthiverma03@gmail.com>
2018-09-18 17:38:49 +0530

    searchpeople

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { GrdFilterPipe } from './grd-filter.pipe';
+import { SearchpeopleComponent } from './searchpeople/searchpeople.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchpeopleComponent,
+    GrdFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'customer-link', component: SearchpeopleComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
